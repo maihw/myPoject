5dcaee6aefaf33c150bea78113c600975f52feca
maihw <704687027@qq.com>
2020-08-28 13:53:04 +0800

    tem

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("helo, world!")
+def detail(request):
+    book_list = Book.object.order_by('-pub_date')[:5]
+    context = {'book_list': book_list}
+    return render(request, 'myApp/detail.html', context)
